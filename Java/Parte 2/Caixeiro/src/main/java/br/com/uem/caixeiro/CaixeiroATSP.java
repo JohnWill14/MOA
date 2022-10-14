e91bb016f499a0ab3e15b701bb1029b0eddc857e
John William <johnwill.v.2017@gmail.com>
2022-10-14 20:04:09 -0300

    add heuristica de melhoramento op-2

--- Java/Parte 2/Caixeiro/src/main/java/br/com/uem/caixeiro/CaixeiroATSP.java
+++ Java/Parte 2/Caixeiro/src/main/java/br/com/uem/caixeiro/CaixeiroATSP.java
@@ -19,9 +19,6 @@ public class CaixeiroATSP implements Caixeiro{
         visitado = new int[this.dimension];
 
     }
-
-
-
     private  List<Integer> opt(int s){
         int pontoVerificar = s;
         List<Integer> rotaMinimizada = new ArrayList<>();
@@ -35,11 +32,6 @@ public class CaixeiroATSP implements Caixeiro{
 
         pontoMenor = pontoMaisProximo(s, rotaMinimizada);
         rotaMinimizada.add(pontoMenor);
-
-        System.out.println("oi");
-        rotaMinimizada.stream()
-                .forEach(System.out::println);
-        System.out.println("fim /=");
 
         int tamanhoRotaMinimizada = this.dimension -2;
 
@@ -55,7 +47,6 @@ public class CaixeiroATSP implements Caixeiro{
                 for(int j = 0; j<rotaMinimizada.size()-1; j++ ){
                     int k1 = rotaMinimizada.get(j);
                     int k2 = rotaMinimizada.get(j+1);
-                    System.out.println(k1+" "+k2);
                     int custoNovaRota = dist[k1][i] + dist[k2][i] - dist[k1][k2];
 
                     if(custoNovaRota<custo){
@@ -76,6 +67,11 @@ public class CaixeiroATSP implements Caixeiro{
 
         rotaMinimizada.add(s);
 
+        System.out.println(rotaMinimizada);
+
+        rotaMinimizada = melhoramento2Opt(rotaMinimizada);
+
+
         return rotaMinimizada;
     }
 
@@ -93,12 +89,68 @@ public class CaixeiroATSP implements Caixeiro{
             }
         }
 
+
         return pontoMaisProximo;
     }
     private void inserirPontoApos(int pontoInserir, List<Integer> rotaMinimizada, int pontoMenor){
         int lastIndexOf = rotaMinimizada.lastIndexOf(pontoMenor);
         lastIndexOf++;
         rotaMinimizada.add(lastIndexOf, pontoInserir);
+    }
+
+    private List<Integer> melhoramento2Opt(List<Integer> rota){
+
+        int custoMelhorRota = custoRota(rota);
+        boolean encontrouMelhorRota = true;
+        int tamanhoRota = rota.size();
+
+        while(encontrouMelhorRota){
+            encontrouMelhorRota = false;
+
+            for(int i=1; i< tamanhoRota - 1;i++ ){
+                for(int j = i+2; j<rota.size()-1;j++){
+                    List<Integer> novaRota = opt2Swap(i, j, rota);
+                    int custNovaRota = custoRota(novaRota);
+                    if(custNovaRota<custoMelhorRota){
+                        rota = novaRota;
+                        custoMelhorRota = custNovaRota;
+                        encontrouMelhorRota = true;
+                    }
+
+                }
+            }
+        }
+
+        return rota;
+    }
+
+    private List<Integer> opt2Swap(int i, int j, List<Integer> rota){
+        List<Integer> parteInicial = rota.subList(0, i+1);
+
+        List<Integer> subRota = new ArrayList<>(rota.subList(i + 1, j + 1));
+        Collections.reverse(subRota);
+
+
+        List<Integer> rotaFinal = rota.subList(j+1, rota.size());
+
+        List<Integer> novaRota =  new ArrayList<>();
+        novaRota.addAll(parteInicial);
+        novaRota.addAll(subRota);
+        novaRota.addAll(rotaFinal);
+
+        return novaRota;
+    }
+
+    private int custoRota(List<Integer> rota){
+        int custo = 0;
+
+        for(int i = 0; i<rota.size()-1;i++){
+            int v1 = rota.get(i);
+            int v2 = rota.get(i+1);
+            custo += dist[v1][v2];
+        }
+
+        return custo;
     }
     @Override
     public void solve() {
@@ -111,18 +163,10 @@ public class CaixeiroATSP implements Caixeiro{
         System.out.println(collect);
 
         Set<Integer> set = new HashSet<>(opt);
-        System.out.println("tam: "+set);
+        System.out.println("tam: "+set.size());
 
-        int custo = 0;
 
-        for(int i = 1;i<opt.size();i++){
-            Integer k = opt.get(i);
-            Integer j = opt.get(i-1);
-
-            custo += dist[j][k];
-        }
-
-        System.out.println("custo: "+custo);
+        System.out.println("custo: "+custoRota(opt));
     }
 
     @Override
